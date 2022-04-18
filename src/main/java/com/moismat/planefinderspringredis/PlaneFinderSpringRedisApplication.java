f93c153555f9e00f0adc9f23c0b159283f601bff
mo.ismat <mo.ismat@auto1.com>
2022-04-18 18:32:07 +0200

    init commit

---
+++ src/main/java/com/moismat/planefinderspringredis/PlaneFinderSpringRedisApplication.java
@@ -0,0 +1,11 @@
+package com.moismat.planefinderspringredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaneFinderSpringRedisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PlaneFinderSpringRedisApplication.class, args);
+	}
+}
---
+++ src/test/java/com/moismat/planefinderspringredis/PlaneFinderSpringRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moismat.planefinderspringredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneFinderSpringRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
