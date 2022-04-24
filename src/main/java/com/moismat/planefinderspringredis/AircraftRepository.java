3d55003c0987cc52748c6afe11137ad895b61ed7
Mo Ismat <mo.ismat@auto1.com>
2022-04-24 17:47:37 +0200

    02 convert from template to repository (#2)

---
+++ src/main/java/com/moismat/planefinderspringredis/AircraftRepository.java
@@ -0,0 +1,6 @@
+package com.moismat.planefinderspringredis;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AircraftRepository extends CrudRepository<Aircraft, Long> {
+}
